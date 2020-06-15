8cef7b0013ad0218602c9bd30edfc540ca38495a
Brodie Robertson <brodierobertson54321@gmail.com>
2020-06-16 00:01:54 +0930

    Add notification_id back into the header

--- callbacks.h
+++ callbacks.h
@@ -3,6 +3,8 @@
 #include <gio/gio.h>
 #include <glib.h>
 
+extern unsigned int notification_id;
+
 void method_handler(GDBusConnection*, const gchar*, const gchar*, const gchar*,
     const gchar*, GVariant*, GDBusMethodInvocation*, gpointer);
 
