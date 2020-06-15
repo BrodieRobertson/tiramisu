63944b7de443879f444b8fa86df56fd555baba64
Brodie Robertson <brodierobertson54321@gmail.com>
2020-06-15 23:52:46 +0930

    Turn every output into a key value pair
    This makes it significantly easier to actually query for the
    data you care about rather than just assuming which line it should
    be on.

--- format.c
+++ format.c
@@ -3,6 +3,14 @@
 #include "config.h"
 #include "format.h"
 
+/* I'm not a C programmer, there's a probably a better way to do this */
+const char *APP_NAME_NAME = "APP_NAME:";
+const char *APP_ICON_NAME = "APP_ICON:";
+const char *REPLACES_ID_NAME = "REPLACES_ID:";
+const char *TIMEOUT_NAME = "TIMEOUT:";
+const char *SUMMARY_NAME = "SUMMARY:";
+const char *BODY_NAME = "BODY:";
+
 void output_notification(gchar *app_name, guint32 replaces_id, gchar *app_icon,
     gchar *summary, gchar *body, GVariant *actions, GVariant *hints,
     gint32 timeout) {
@@ -11,21 +19,22 @@ void output_notification(gchar *app_name, guint32 replaces_id, gchar *app_icon,
     /* 2048 characters should be significantly long enough*/
     char *string = (char *)calloc(2048, sizeof(char));
 
-    strcat(string, app_name);
+    strcat(string, APP_NAME_NAME);
+		strcat(string, app_name);
 
 #ifdef RECEIVE_APP_ICON
-    sprintf(string, "%s%s%s", string, OUTPUT_DELIMITER, app_icon);
+    sprintf(string, "%s%s%s%s", string, OUTPUT_DELIMITER, APP_ICON_NAME, app_icon);
 #endif
 
     /* TODO: actions */
     /* TODO: hints */
 
 #ifdef RECEIVE_REPLACES_ID
-    sprintf(string, "%s%s%lu", string, OUTPUT_DELIMITER, replaces_id);
+    sprintf(string, "%s%s%s%lu", string, OUTPUT_DELIMITER, REPLACES_ID_NAME, replaces_id);
 #endif
 
-    sprintf(string, "%s%s%d%s%s%s%s", string, OUTPUT_DELIMITER,
-        timeout, OUTPUT_DELIMITER, summary, OUTPUT_DELIMITER, body);
+    sprintf(string, "%s%s%s%d%s%s%s%s%s%s", string, OUTPUT_DELIMITER, TIMEOUT_NAME, timeout,
+				OUTPUT_DELIMITER, SUMMARY_NAME, summary, OUTPUT_DELIMITER, BODY_NAME, body);
 
     printf("%s\n", string);
     fflush(stdout);
